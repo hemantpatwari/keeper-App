6798fcd36ae08508925e0ae3b296dda33c416861
hemant patwari <hamentpatwari@gmail.com>
2021-07-30 11:26:18 +0530

    Updates

---
+++ precache-manifest.87acf12b2c898474b1c96e44ca1a8fd9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd61eb84d6a4bd2d22c3d334904448e",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "599f727a25224ca4cb61",
+    "url": "/keeper-app/static/js/2.14ebd957.chunk.js"
+  },
+  {
+    "revision": "f2fb5c4922f6501eac7a",
+    "url": "/keeper-app/static/js/main.3b638d9a.chunk.js"
+  },
+  {
+    "revision": "ed9243ad83c12b8fd98c",
+    "url": "/keeper-app/static/js/runtime-main.604284a8.js"
+  }
+]);
